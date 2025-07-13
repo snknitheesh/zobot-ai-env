f225f6be7cbc29dc044c0aa22bf160e2e9400f54
Snknitheesh <snknitheesh@gmail.com>
2025-07-13 00:08:25 +0000

    fixed alias

--- .bin/alias.sh
+++ .bin/alias.sh
@@ -50,6 +50,6 @@ alias ws="cd /home/zozo/zobot_ws"
 alias rosws="cd /home/zozo/zobot_ws/ros"
 alias logs="cd /home/zozo/.ros_builds/log"
 alias builds="cd /home/zozo/.ros_builds"
-alias cdmount="cd /home/zozo/mount"
+alias cdmount="cd /home/zozo/zobot_ws/mount"
 alias cdcave="cd /home/zozo/cave"
 
